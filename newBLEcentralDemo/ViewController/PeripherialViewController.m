9a0839f0b2445ebfc8f11a1b1e9295b288248942
Ivan_deng <dlfkid@icloud.com>
2018-12-18 16:01:32 +0800

    添加设备服务列表

--- newBLEcentralDemo/ViewController/PeripherialViewController.m
+++ newBLEcentralDemo/ViewController/PeripherialViewController.m
@@ -8,13 +8,23 @@
 
 #import "PeripherialViewController.h"
 
-@interface PeripherialViewController()
+// helpers
+#import <CoreBluetooth/CoreBluetooth.h>
+#import <Masonry/Masonry.h>
+#import "UIDevice+DeviceInfo.h"
+
+@interface PeripherialViewController()<UITableViewDelegate, UITableViewDataSource>
 
 @property (nonatomic, strong) CBPeripheral *perpherial;
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
+static NSString * const serviceCellReuseIdentifier = @"PeripheralViewController.tableView.reuseIdentifier";
+
 @implementation PeripherialViewController
+
+#pragma mark - InitialSetup
 
 - (instancetype)initWithPerpherial:(CBPeripheral *)perpherial {
   self = [super initWithNibName:nil bundle:nil];
@@ -22,6 +32,55 @@
     _perpherial = perpherial;
   }
   return self;
+}
+
+- (UITableView *)tableView {
+  if (!_tableView) {
+    _tableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+    _tableView.delegate = self;
+    [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:serviceCellReuseIdentifier];
+  }
+  return _tableView;
+}
+
+
+- (void)setupContent {
+  self.view.backgroundColor = [UIColor whiteColor];
+  [self.view addSubview:self.tableView];
+}
+
+#pragma mark - LifeCycle
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  self.navigationItem.title = self.perpherial.name;
+  [self setupContent];
+}
+
+- (void)viewWillLayoutSubviews {
+  [super viewWillLayoutSubviews];
+  [self.tableView mas_makeConstraints:^(MASConstraintMaker *make) {
+    make.top.equalTo(@([UIDevice navigationBarAndStatusBarHeight]));
+    make.left.right.equalTo(@0);
+    make.bottom.equalTo(@([UIDevice bottomIndicatior]));
+  }];
+}
+
+#pragma mark - TableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+  return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+  return self.perpherial.services.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:serviceCellReuseIdentifier];
+  CBService *service = self.perpherial.services[indexPath.row];
+  cell.textLabel.text = service.UUID.UUIDString;
+  return cell;
 }
 
 @end
